c8c763cd92ca919fbc54cfb83d6fab88a1b007e1
Landan <LandanParker@gmail.com>
2023-06-05 20:38:14 -0500

    .

--- Obsidian.Stripped.BusinessLogic/Actions/Commands/Login.cs
+++ Obsidian.Stripped.BusinessLogic/Actions/Commands/Login.cs
@@ -8,5 +8,5 @@ public record Login : IRequest
 
 public record LoginHandler(IDictionary<string,string> A) : VoidHandlerBase<Login>
 {
-    protected override Action<IRequest> HandleAction => _ => A;
+    protected override Action<IRequest> HandleAction => _ => { };
 }
